a0162426639d9a816c49a96f2e4fbd628467cffe
Zachary Devine <zach.devine17@gmail.com>
2025-05-14 10:50:46 -0700

    commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let code1;
+code1 = 7+3;
+
+const code2 = 120/2-20;
+
+const code3 =  3%2*39;
+
+const message = `The vault has been secured. The combination is: `
+const codeA = code1 + `-` + code2 + `-` + code3;
+const codeB = `${code1}-${code2}-${code3}`;
+
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
